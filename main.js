4a3c009eaf8bba5f20a20b45b2d914b5e2da14b7
victorpreto044 <127959288+victorpreto044@users.noreply.github.com>
2024-09-17 11:19:12 -0300

    hjuyuyuuyyuuyiuuoyoyiuyoiuyy9797ihjjg

--- main.js
+++ main.js
@@ -22,7 +22,7 @@ function diminuiTamanho() {
     geraSenha();
 }
 function aumentaTamanho() {
-    if (tamanhoSenha < 20) {
+    if (tamanhoSenha < 24) {
         // tamanhoSenha = tamanhoSenha+1;
         tamanhoSenha++;
     }
